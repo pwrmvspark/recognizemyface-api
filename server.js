b29933f3e292f50ff688bf13220c8998f0c0d4f7
pwrmvspark <danielparkonline@gmail.com>
2019-11-20 09:59:08 -0800

    routes updated, cors + bcrypt packages installed

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors())
 
 const database = {
   users: [
@@ -33,7 +35,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email &&
       req.body.password === database.users[0].password) {
-    res.json('success')
+    res.json('success') 
   } else {
     res.status(400).json('error logging in')
   }
@@ -41,11 +43,13 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req,res) => {
   const { name, email, password } = req.body
+  bcrypt.hash(password, null, null, function(err, hash) {
+    console.log(hash)
+});
   database.users.push({
       id: '125',
       name: name,
       email: email,
-      password: password,
       entries: 0,
       joined:   Date()
   })
