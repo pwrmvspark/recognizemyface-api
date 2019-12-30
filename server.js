260bf1f02b632fed5bac5ae3d6327e66a168593a
daniel park <danielparkonline@gmail.com>
2019-12-30 12:55:51 -0800

    adding heroku PORT

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ const db  = knex({
   }
 });
 
-db.select('*').from('users').then(data => {
-  // console.log(data)
-})
-
 console.log(db.select('*').from('users'))
 
 app.use(bodyParser.json())
@@ -36,6 +32,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3001, () => {
-  console.log('app is running on port 3001')
+app.listen(process.env.PORT ||  3001, () => {
+  console.log('app is running on port ${process.env.PORT}')
 })
